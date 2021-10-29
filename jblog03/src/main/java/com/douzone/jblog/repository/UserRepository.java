24043a65b7be3341315a3e324d41d7d253b00e3c
stpn94 <stpn94@gmail.com>
2021-10-29 18:38:41 +0900

    userRepo 설정

---
+++ jblog03/src/main/java/com/douzone/jblog/repository/UserRepository.java
@@ -0,0 +1,50 @@
+package com.douzone.jblog.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.douzone.jblog.vo.UserVo;
+
+@Repository
+public class UserRepository {
+
+
+	@Autowired
+	private SqlSession sqlSession;
+
+
+//	@Autowired
+//	CategoryService categoryService;
+//	public void insertUser(UserVo userVo, BlogVo blogVo, CategoryVo categoryVo, PostVo postVo) {
+//		sqlSession.insert("user.insert", userVo);
+//		sqlSession.insert("blog.insert", blogVo);
+//		sqlSession.insert("category.insert", categoryVo);
+//
+//		CategoryVo categoryVoOne = categoryService.findByOne(userVo.getId());
+//		postVo.setCategory_no(categoryVoOne.getNo());
+//		
+//		sqlSession.insert("post.insert", postVo);
+//	}
+	
+	public void insertUser(UserVo userVo) {
+		sqlSession.insert("user.insert", userVo);
+	}
+
+	public UserVo getUser(String id) {
+		return sqlSession.selectOne("user.findById", id);
+	}
+
+	public UserVo getUser(String id, String password) {
+
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("i", id);
+		map.put("p", password);
+		return sqlSession.selectOne("user.findByIdAndPassword", map);
+
+	}
+
+}
