7e914b5d4d929780e027326eb97b37e2c390106a
stpn94 <stpn94@gmail.com>
2021-10-29 17:31:27 +0900

    jblog #Vo설정 완료

---
+++ jblog03/src/main/java/com/douzone/jblog/vo/CategoryVo.java
@@ -0,0 +1,51 @@
+package com.douzone.jblog.vo;
+
+public class CategoryVo {
+
+	private int no;
+	private String name;
+	private String desc;
+	private String id;
+	private int count;
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+}
